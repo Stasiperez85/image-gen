8cbdaab6506abd85ead39077aa0e0cb31b2e4597
Stasiperez85 <Stasiperez85@gmail.com>
2022-08-17 21:45:29 -0600

    finalize search javascript function

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
 const input = document.getElementById('input');
-const grid = document.getElementsByClassName('grid') [0];
+const grid = document.getElementsByClassName('grid')[0];
 
-window.addEventListener('load', dayNightMode
-)
-input.addEventListener('keydown', function(event){
-    if(event.key === "Enter")
+window.addEventListener('load', dayNightMode)
+
+input.addEventListener('keydown', function (event) {
+    if (event.key === "Enter")
         loadImg();
 })
 
-function loadImg(){
-    
+function loadImg() {
+    removeImages();
+
+    const url = 'https://api.unsplash.com/search/photos/?query=' + input.value + '&per_page=9&client_id=MeGnfJkrJR30RWn6MU6YiW7kchxls-VjHAvKAcgEUXo'
+
+    fetch(url)
+
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            else
+                alert(response.status)
+        })
+
+        .then(data => {
+            const imageNodes = [];
+            for (let i = 0; i < data.results.length; i++) {
+                imageNodes[i] = document.createElement('div');
+                imageNodes[i].className = 'img';
+                imageNodes[i].style.backgroundImage = 'url(' + data.results[i].urls.raw + ')';
+                imageNodes[i].addEventListener('dblclick', function () {
+                    window.open(data.results[i].links.download, '_blank');
+                })
+                grid.appendChild(imageNodes[i]);
+            }
+        })
 }
 
-function dayNightMode(){
+function removeImages() {
+    grid.innerHTML = "";
+
+
+}
+
+function dayNightMode() {
     const date = new Date();
     const hour = date.getHours();
 
-    if(hour >= 7 && hour <= 19) {
+    if (hour >= 7 && hour <= 19) {
         document.body.style.backgroundColor = "whitesmoke";
         document.body.style.color = 'black';
     }
-    else{
+    else {
         document.body.style.backgroundColor = "black";
-        document.body.style.backgroundColor = "white";
+        document.body.style.color = "white";
     }
 }
+
